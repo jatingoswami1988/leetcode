134899b2e581ab1ea6483bca1acbba2eaa771d7f
INFOCORP\jatin.goswami <jatin.goswami@ihsmarkit.com>
2023-07-16 12:40:31 +0530

    Leetcode problem no 131

---
+++ src/main/backtracking/Porblem_131PalindromePartitioning.java
@@ -0,0 +1,47 @@
+package main.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Porblem_131PalindromePartitioning {
+
+	public static void main(String[] args) {
+
+		String s = "aa";
+		partition(s);
+	}
+
+	private static void partition(String s) {
+		List<List<String>> result = new ArrayList<>();
+		palindromePartitioning(s, result, new ArrayList<>());
+		System.out.println(result);
+	}
+
+	private static void palindromePartitioning(String str, List<List<String>> result, List<String> temp) {
+
+		if (str.length() == 0) {
+			result.add(new ArrayList<>(temp));
+			return;
+		} else {
+
+			for (int i = 0; i < str.length(); i++) {
+				String prefix = str.substring(0, i + 1);
+				String restOfTheString = str.substring(i + 1);
+				if (pallindrome(prefix)) {
+					temp.add(prefix);
+					palindromePartitioning(restOfTheString, result, new ArrayList<>(temp));
+					temp.remove(temp.size() - 1);
+				}
+			}
+		}
+	}
+
+	private static boolean pallindrome(String prefix) {
+		for (int i = 0; i < prefix.length() / 2; i++) {
+			if (prefix.charAt(i) != prefix.charAt(prefix.length() - i - 1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
